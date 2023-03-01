009ec2019cac1e325a6c4d4014a42910dc852700
Bernie Lam <bernie@Berniede-MBP.cust.communityfibre.co.uk>
2023-03-01 09:33:43 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class App extends Component {
-  state = {};
+  state = { weather: {} };
 
   async componentDidMount() {
     //first situation (if success) //step 3
@@ -23,14 +23,43 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state);
-    const { list } = this.state.weather;
+    const { list, city } = this.state.weather;
+    // const { name, country } = this.state.weather.city;
+
+    console.log(this.state.weather);
+    console.log(this.state.weather.city);
+
+    if (!list) {
+      return <h1>Loading......</h1>;
+    } // why this one needed, because it need to return in the render method if the weather data has not yet arrived, otherwise it will get error
 
     return (
-      // <h1>hi</h1>
+      //----- call the data you want and use //step 4
       <>
-        {list.map((item) => {
-          return `<p>${new Date(item.dt * 1000).toLocaleString()}</p>`;
+        {list.map((item, index) => {
+          if (index % 4 !== 0) {
+            return;
+          } //----- if it is not a point in the array, skip it // step 5
+
+          return (
+            <div class="small-container">
+              <div class="forecastItem">
+                <div class="time">
+                  <p>{new Date(item.dt * 1000).toLocaleString()}</p>
+                </div>
+                <div class="temp">
+                  <p>{Math.round(item.main.temp - 273.15)}&deg;C</p>
+                </div>
+                <div class="status">
+                  <p>{item.weather[0].main}</p>
+                </div>
+
+                {/* <div class="icon"><img src="https://openweathermap.org/img/wn/${
+       item.weather[0].icon
+     }.png" alt="${item.weather[0].main}"></div> */}
+              </div>
+            </div>
+          );
         })}
       </>
     );
